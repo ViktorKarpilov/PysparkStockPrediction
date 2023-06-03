4c7668c5410e8f536cb0b7931a7aafe894f807a1
Viktor Karpilov <vkarpil@softserveinc.com>
2023-06-03 16:36:40 +0300

    Add endpoints

--- application/currencyService.py
+++ application/currencyService.py
@@ -1,19 +1,29 @@
 from model.model_executor import model_executor
 from yahooquery import Ticker
+from datetime import datetime, timedelta
 
 class CurrencyService():
     def __init__(this):
-        __executor = model_executor
+        this.__executor = model_executor
 
-    def get_prediction(this, ticker, period):
-        aapl = Ticker(ticker)
+    def get_prediction(this, ticker, hours):
+        ticker = Ticker(ticker, asynchronous=True)
 
-        # Summary Information
-        details = aapl.summary_detail[ticker]
-        traget = [(details["open"], details["dayHigh"], details["dayLow"], details["ask"], details["volume"])]
-        __executor.predict(traget)
+        details = ticker.history(period='1h', interval='1h')
+        target = details.tail(1)
+        date = target.index[0][1]
 
-        return __executor.predict(traget, period)
+        predict_y = this.__executor.predict(target["open"].values[0],
+                                            target["high"].values[0],
+                                            target["low"].values[0],
+                                            target["close"].values[0],
+                                            hours)
+        result = []
+        for y in predict_y:
+            date += timedelta(hours=1)
+            result.append([y, date])
+
+        return result
 
 currency_service = CurrencyService()
         
--- model/model_executor.py
+++ model/model_executor.py
@@ -8,20 +8,22 @@ from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import DecisionTreeRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml import PipelineModel
 
 class ModelExecutor():
     def __init__(this):
-        spark = SparkSession.builder \
-            .appName("Decision Tree Regression with PySpark") \
-            .getOrCreate()
-        # __model = DecisionTreeRegressor.load("./tree_regressor")
+        print("init")
+        # spark = SparkSession.builder \
+        #     .appName("Decision Tree Regression with PySpark") \
+        #     .getOrCreate()
+        # __model = PipelineModel.load("./model/tree_regressor")
 
-    def predict(this, previousData, period):
-        result = [previousData]
+    def predict(this, open, hight, low, closed, hours):
+        # result = [previousData]
 
-        for i in range(period):
-            result.append(__model.transform(result[-1]))
+        # for i in range(period):
+        #     result.append(__model.transform(result[-1]))
 
-        return result
+        return [1,2,3,4,5,6,7,8,9,1,4]
 
 model_executor = ModelExecutor()

9f14750127e37e536bc36b0c70b904c6714c0744
Viktor Karpilov <vkarpil@softserveinc.com>
2023-06-03 16:37:02 +0300

    add infrastructure

---
+++ controllers/tickers_controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from flask.views import MethodView
+from application.currencyService import currency_service
+from domain.tickers import tickers
+
+class TickersController(MethodView):
+    def get(self):
+        return tickers
+
+
+tickers_controller_bp = Blueprint('tickers_controller', __name__)
+ticker_view = TickersController.as_view('tickers_view')
+tickers_controller_bp.add_url_rule('/tickers', view_func=ticker_view, methods=['GET'])

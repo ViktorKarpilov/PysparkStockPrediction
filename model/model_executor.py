d2e609cfb039e24e97462596fbc499b161681719
HLIB SIN <sinixyowo@gmail.com>
2023-06-04 18:37:08 +0300

    modelexcutor

--- model/model_executor.py
+++ model/model_executor.py
@@ -1,29 +1,63 @@
-from pyspark.sql import SparkSession
-from pyspark.ml import Pipeline
+from pyspark.sql import SparkSession, types
+from pyspark.ml import Pipeline, PipelineModel
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import DecisionTreeRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.sql import SparkSession
-from pyspark.ml import Pipeline
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.regression import DecisionTreeRegressor
-from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml import PipelineModel
+from pyspark.sql.functions import lit
+
+import pandas as pd
+from datetime import timedelta
+
 
 class ModelExecutor():
-    def __init__(this):
-        print("init")
-        # spark = SparkSession.builder \
-        #     .appName("Decision Tree Regression with PySpark") \
-        #     .getOrCreate()
-        # __model = PipelineModel.load("./model/tree_regressor")
+    def __init__(self):
+        self.__spark = SparkSession.builder \
+            .appName("Decision Tree Regression with PySpark") \
+            .getOrCreate()
+        self.__model = PipelineModel.load("./model/tree_regressor")
 
-    def predict(this, data: dict, hours: int):
-        # result = [previousData]
+    def __prepare_df(self, data):
+        df = pd.DataFrame(data)
+        df['DateTime'] = pd.to_datetime(df['DateTime'])
+        df[["Open", "High", "Low", "Close", "Volume"]] = df[["Open", "High", "Low", "Close", "Volume"]].pct_change()
+        df["EMA-8"] = df["Close"].ewm(span=8, adjust=False).mean()
+        df["EMA-32"] = df["Close"].ewm(span=32, adjust=False).mean()
+        df.dropna(inplace=True)
 
-        # for i in range(period):
-        #     result.append(__model.transform(result[-1]))
+        spark_df = self.__spark.createDataFrame(df)
+        spark_df = spark_df.orderBy("DateTime", ascending=False).limit(1)
+        spark_df = spark_df.withColumn("Target", lit(None).cast(types.NullType()))
 
-        return [1,2,3,4,5,6,7,8,9,1,4]
+        return spark_df
+    
+    def __get_next_df(self, prediction: dict):
+        price = prediction['prediction']
+        columns = ['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume', 'EMA-32', 'EMA-8', 'Target']
+        return self.__spark.createDataFrame([
+            (prediction['DateTime'], price, price, price, price, prediction['Volume'], prediction['EMA-32'], prediction['EMA-8'], None)
+        ]).toDF(*columns)
+
+    def predict(self, data: dict, hours: int):
+        '''
+        data = {
+            'DateTime': [...],
+            'Open': [...],
+            'High': [...],
+            'Low': [...],
+            'Volume': [...]
+        }
+        '''
+        result = []
+
+        df = self.__prepare_df(df)
+        prediction = [r.asDict() for r in self.__model.transform(df).collect()][0]
+        result.append(prediction['prediction'])
+
+        for _ in range(hours - 1):
+            next_df = self.__get_next_df(prediction)
+            prediction = [r.asDict() for r in self.__model.transform(next_df).collect()][0]
+            result.append(prediction['prediction'])
+
+        return result
 
 model_executor = ModelExecutor()
